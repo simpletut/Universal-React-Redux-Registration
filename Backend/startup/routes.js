3e111f3abc217bffeddd93e357a8c5c878664888
Ashley Bibizadeh <ashleybibizadeh@Ashleys-MacBook-Pro.local>
2018-09-30 16:18:56 +0100

    pre release updates

--- Backend/startup/routes.js
+++ Backend/startup/routes.js
@@ -3,10 +3,11 @@ const error = require('./../middleware/error');
 const users = require('./../routes/users');
 const auth = require('./../routes/auth');
 const notFound = require('./../routes/notFound');
+
 module.exports = function (app) {
     app.use(express.json());
     app.use('/api/users', users);
     app.use('/api/auth', auth);
-    app.use('*', notFound);
     app.use(error);
+    app.all('*', notFound);
 }
