25c3f5ca88788181113995bb2cb9daa1c18bde6c
Ashley Bibizadeh <ashleybibizadeh@Ashleys-MacBook-Pro.local>
2018-11-23 19:45:15 +0000

    New posts route (Backend) and users pagination fix (active route)

--- Backend/startup/db.js
+++ Backend/startup/db.js
@@ -3,6 +3,6 @@ const winston = require('winston');
 const config = require('config');
 
 module.exports = function () {
-    mongoose.connect(config.get('db'))
+    mongoose.connect(config.get('db'), { useNewUrlParser: true })
     .then(() => winston.info('Connection to MongoDB established successfully...'));
 }
--- Backend/startup/routes.js
+++ Backend/startup/routes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const error = require('./../middleware/error');
 const users = require('./../routes/users');
+const posts = require('./../routes/posts');
 const auth = require('./../routes/auth');
 const notFound = require('./../routes/notFound');
 
 module.exports = function (app) {
     app.use(express.json());
     app.use('/api/users', users);
+    app.use('/api/posts', posts);
     app.use('/api/auth', auth);
     app.use(error);
     app.all('*', notFound);
